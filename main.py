b7fdcb1d322433160904f7734fee9cbc35c2ed76
Yudaotor <yudaotor@qq.com>
2023-03-15 15:06:57 +0800

    增加对于谷歌,火狐浏览器的支持

--- main.py
+++ main.py
@@ -2,11 +2,9 @@ import argparse
 import logging
 import sys
 from pathlib import Path
-
-from selenium.webdriver import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.edge.options import Options
-from selenium.webdriver.edge.service import Service
+from Handler import Handler
+from VersionManager import VersionManager
+from Webdriver import Webdriver
 from selenium import webdriver
 import time
 from rich import print
@@ -17,66 +15,39 @@ from Logger import Logger
 # coding:utf-8
 
 
-def changePwd(log: logging.Logger, username, password, newPassword):
+def init() -> tuple[logging.Logger, Config, webdriver.Edge]:
+    global driver, config, log
     try:
-        # login
-        driver.find_element(by=By.XPATH, value='/html/body/div[2]/div/div/div[2]/div/div/div[2]/div/div/div/div[1]/div/input').send_keys(Keys.CONTROL, 'a')
-        driver.find_element(by=By.XPATH, value='/html/body/div[2]/div/div/div[2]/div/div/div[2]/div/div/div/div[1]/div/input').send_keys(username)
-        driver.find_element(by=By.XPATH, value='/html/body/div[2]/div/div/div[2]/div/div/div[2]/div/div/div/div[2]/div/input').send_keys(Keys.CONTROL, 'a')
-        driver.find_element(by=By.XPATH, value='/html/body/div[2]/div/div/div[2]/div/div/div[2]/div/div/div/div[2]/div/input').send_keys(password)
-        buttonLogin = driver.find_element(by=By.XPATH, value='/html/body/div[2]/div/div/div[2]/div/div/button')
-        time.sleep(1)
-        driver.execute_script("arguments[0].click();", buttonLogin)
-        # change password
-        driver.find_element(by=By.XPATH, value='//*[@id="riot-account"]/div/div[2]/div/div[2]/div[1]/div/div/input').send_keys(password)
-        driver.find_element(by=By.XPATH, value='//*[@id="riot-account"]/div/div[2]/div/div[2]/div[3]/div/input').send_keys(newPassword)
-        driver.find_element(by=By.XPATH, value='//*[@id="riot-account"]/div/div[2]/div/div[2]/div[2]/div/input').send_keys(newPassword)
-        driver.find_element(by=By.XPATH, value='//*[@id="riot-account"]/div/div[1]/p').click()
-        driver.find_element(by=By.XPATH, value='//*[@id="riot-account"]/div/div[2]/div/div[3]/button[2]').click()
-        time.sleep(2)
-        log.info(username + " Success")
-        print(username + " [green]Success")
-        # logout
-        driver.find_element(by=By.XPATH, value='//*[@id="riotbar-account-bar"]/div/div').click()
-        driver.find_element(by=By.XPATH, value='//*[@id="riotbar-account-dropdown-links"]/a[3]').click()
-        time.sleep(2)
-    except Exception as e:
-        log.error(username + " Fail")
-        print(username + " [red]Fail")
-
-
-def init() -> tuple[logging.Logger, Config]:
-    try:
-        global driver
         parser = argparse.ArgumentParser(description='英雄联盟外服自动改密码')
         parser.add_argument('-c', '--config', dest="configPath", default="./config.yaml", help='Path to a custom config file')
         args = parser.parse_args()
         Path("./logs/").mkdir(parents=True, exist_ok=True)
         log = Logger.createLogger()
         config = Config(log, args.configPath)
-        o = Options()
-        o.add_experimental_option('excludeSwitches', ['enable-logging'])
-        s = Service(config.msedgedriverPath)
-        driver = webdriver.Edge(service=s, options=o)
+        try:
+            driver = Webdriver(browser=config.browser, driverPath=config.webDriverPath).createWebdriver()
+        except Exception as ex:
+            print(ex)
+            print("[red]找不到对应的webDriver!请检查路径是否正确或是否适配自己浏览器版本\n按任意键退出...")
+            input()
+            exit()
         # 载入网页
-        driver.get(
-            'https://auth.riotgames.com/login#acr_values=urn%3Ariot%3Agold&client_id=accountodactyl-prod&redirect_uri'
-            '=https%3A%2F%2Faccount.riotgames.com%2Foauth2%2Flog-in&response_type=code&scope=openid%20email%20profile'
-            '%20riot%3A%2F%2Friot.atlas%2Faccounts.edit%20riot%3A%2F%2Friot.atlas%2Faccounts%2Fpassword.edit%20riot'
-            '%3A%2F%2Friot.atlas%2Faccounts%2Femail.edit%20riot%3A%2F%2Friot.atlas%2Faccounts.auth%20riot%3A%2F'
-            '%2Fthird_party.revoke%20riot%3A%2F%2Fthird_party.query%20riot%3A%2F%2Fforgetme%2Fnotify.write%20riot%3A'
-            '%2F%2Friot.authenticator%2Fauth.code%20riot%3A%2F%2Friot.authenticator%2Fauthz.edit%20riot%3A%2F%2Frso'
-            '%2Fmfa%2Fdevice.write%20riot%3A%2F%2Friot.authenticator%2Fidentity.add&state=547c8cd2-9eb0-4302-b9b2'
-            '-f29ee843a4bd&ui_locales=zh-Hans')
         driver.implicitly_wait(10)  # 隐式等待时间
         driver.maximize_window()  # 最大化窗口
     except Exception as e:
         print(e)
-    return log, config
+    return log, config, driver
 
 
-def main(log: logging.Logger, config: Config):
-    print("[yellow]------------程序启动-------------")
+CURRENT_VERSION = 2.0
+log, config, driver = init()
+handle = Handler(log=log, driver=driver)
+if not VersionManager.isLatestVersion(CURRENT_VERSION):
+    print("[red]!!! 新版本可用 !!! 从此处下载: https://github.com/Yudaotor/Riot-Accounts-AutoChangePassword/releases/latest")
+
+
+def main(config: Config):
+    print(f"[yellow]------------v{CURRENT_VERSION}程序启动-------------")
     try:
         with open(config.accountFilePath) as f:
             line = f.readline()
@@ -84,7 +55,9 @@ def main(log: logging.Logger, config: Config):
                 sp = line.split('----')
                 if sp[1][-1] == '\n':
                     sp[1] = sp[1][:-1]
-                changePwd(log, sp[0], sp[1], config.newPassword)
+                    handle.automaticLogIn(sp[0], sp[1])
+                    handle.automaticChangePassword(sp[0], sp[1], config.newPassword)
+                    handle.automaticLogOut()
                 line = f.readline()
     except Exception as e:
         print(e)
@@ -92,11 +65,11 @@ def main(log: logging.Logger, config: Config):
 
 if __name__ == '__main__':
     try:
-        log, config = init()
-        main(log, config)
+        main(config)
     except (KeyboardInterrupt, SystemExit):
         print("[red]------程序退出------")
         sys.exit()
     print("[yellow]------程序退出, 将在3秒后结束------")
     print("[yellow]---本次运行结果可于log文件夹中查看---")
     time.sleep(3)
+    exit()
