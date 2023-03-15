98de1e03ab5412ca3f71c3e585af63e2f9a38009
Yudaotor <yudaotor@qq.com>
2023-03-15 15:55:29 +0800

    update

--- main.py
+++ main.py
@@ -26,6 +26,14 @@ def init() -> tuple[logging.Logger, Config, webdriver.Edge]:
         config = Config(log, args.configPath)
         try:
             driver = Webdriver(browser=config.browser, driverPath=config.webDriverPath).createWebdriver()
+            driver.get('https://auth.riotgames.com/login#acr_values=urn%3Ariot%3Agold&client_id=accountodactyl-prod&redirect_uri'
+                       '=https%3A%2F%2Faccount.riotgames.com%2Foauth2%2Flog-in&response_type=code&scope=openid%20email%20profile'
+                       '%20riot%3A%2F%2Friot.atlas%2Faccounts.edit%20riot%3A%2F%2Friot.atlas%2Faccounts%2Fpassword.edit%20riot'
+                       '%3A%2F%2Friot.atlas%2Faccounts%2Femail.edit%20riot%3A%2F%2Friot.atlas%2Faccounts.auth%20riot%3A%2F'
+                       '%2Fthird_party.revoke%20riot%3A%2F%2Fthird_party.query%20riot%3A%2F%2Fforgetme%2Fnotify.write%20riot%3A'
+                       '%2F%2Friot.authenticator%2Fauth.code%20riot%3A%2F%2Friot.authenticator%2Fauthz.edit%20riot%3A%2F%2Frso'
+                       '%2Fmfa%2Fdevice.write%20riot%3A%2F%2Friot.authenticator%2Fidentity.add&state=547c8cd2-9eb0-4302-b9b2'
+                       '-f29ee843a4bd&ui_locales=zh-Hans')
         except Exception as ex:
             print(ex)
             print("[red]找不到对应的webDriver!请检查路径是否正确或是否适配自己浏览器版本\n按任意键退出...")
@@ -55,9 +63,9 @@ def main(config: Config):
                 sp = line.split('----')
                 if sp[1][-1] == '\n':
                     sp[1] = sp[1][:-1]
-                    handle.automaticLogIn(sp[0], sp[1])
-                    handle.automaticChangePassword(sp[0], sp[1], config.newPassword)
-                    handle.automaticLogOut()
+                    if handle.automaticLogIn(sp[0], sp[1]):
+                        if handle.automaticChangePassword(sp[0], sp[1], config.newPassword):
+                            handle.automaticLogOut()
                 line = f.readline()
     except Exception as e:
         print(e)
@@ -72,4 +80,5 @@ if __name__ == '__main__':
     print("[yellow]------程序退出, 将在3秒后结束------")
     print("[yellow]---本次运行结果可于log文件夹中查看---")
     time.sleep(3)
+    driver.quit()
     exit()
