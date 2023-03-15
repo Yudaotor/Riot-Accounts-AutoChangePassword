0aa6c154f8e934bb022584ff393c1d5bf917f933
Yudaotor <yudaotor@qq.com>
2023-03-15 15:56:01 +0800

    修复由于cookie弹窗导致的错误.

--- Handler.py
+++ Handler.py
@@ -11,44 +11,49 @@ class Handler:
         self.log = log
         self.driver = driver
 
-    def automaticLogIn(self, username, password):
+    def automaticLogIn(self, username, password) -> bool:
         try:
-            self.driver.get('https://auth.riotgames.com/login#acr_values=urn%3Ariot%3Agold&client_id=accountodactyl-prod&redirect_uri'
-                            '=https%3A%2F%2Faccount.riotgames.com%2Foauth2%2Flog-in&response_type=code&scope=openid%20email%20profile'
-                            '%20riot%3A%2F%2Friot.atlas%2Faccounts.edit%20riot%3A%2F%2Friot.atlas%2Faccounts%2Fpassword.edit%20riot'
-                            '%3A%2F%2Friot.atlas%2Faccounts%2Femail.edit%20riot%3A%2F%2Friot.atlas%2Faccounts.auth%20riot%3A%2F'
-                            '%2Fthird_party.revoke%20riot%3A%2F%2Fthird_party.query%20riot%3A%2F%2Fforgetme%2Fnotify.write%20riot%3A'
-                            '%2F%2Friot.authenticator%2Fauth.code%20riot%3A%2F%2Friot.authenticator%2Fauthz.edit%20riot%3A%2F%2Frso'
-                            '%2Fmfa%2Fdevice.write%20riot%3A%2F%2Friot.authenticator%2Fidentity.add&state=547c8cd2-9eb0-4302-b9b2'
-                            '-f29ee843a4bd&ui_locales=zh-Hans')
             time.sleep(1)
             wait = WebDriverWait(self.driver, 10)
-            wait.until(ec.presence_of_element_located((By.XPATH, '//*[@id="2d205e13-dad4-4684-ab7b-39c6089192ae"]/div[2]/button[2]')))
+            self.driver.implicitly_wait(2)
+            cookieButton = self.driver.find_elements(By.XPATH, '/html/body/div[1]/div[2]/div[2]/button[2]')
+            if len(cookieButton) > 0:
+                cookieButton.click()
+            time.sleep(1)
             usernameInput = wait.until(ec.presence_of_element_located((By.CSS_SELECTOR, "input[name=username]")))
             usernameInput.send_keys(Keys.CONTROL, 'a')
             usernameInput.send_keys(username)
+            time.sleep(1)
             passwordInput = wait.until(ec.presence_of_element_located((By.CSS_SELECTOR, "input[name=password]")))
             passwordInput.send_keys(Keys.CONTROL, 'a')
             passwordInput.send_keys(password)
             submitButton = wait.until(ec.presence_of_element_located((By.XPATH, "/html/body/div[2]/div/div/div[2]/div/div/button")))
             self.driver.execute_script("arguments[0].click();", submitButton)
+            return True
         except Exception as e:
             self.log.error(username + " Fail")
             print(username + " [red]Fail")
+            return False
 
-    def automaticChangePassword(self, username, password, newPassword):
+    def automaticChangePassword(self, username, password, newPassword) -> bool:
         try:
+            self.driver.implicitly_wait(10)
             self.driver.find_element(by=By.XPATH, value='//*[@id="riot-account"]/div/div[2]/div/div[2]/div[1]/div/div/input').send_keys(password)
+            time.sleep(1)
             self.driver.find_element(by=By.XPATH, value='//*[@id="riot-account"]/div/div[2]/div/div[2]/div[3]/div/input').send_keys(newPassword)
+            time.sleep(1)
             self.driver.find_element(by=By.XPATH, value='//*[@id="riot-account"]/div/div[2]/div/div[2]/div[2]/div/input').send_keys(newPassword)
+            time.sleep(1)
             self.driver.find_element(by=By.XPATH, value='//*[@id="riot-account"]/div/div[1]/p').click()
             self.driver.find_element(by=By.XPATH, value='//*[@id="riot-account"]/div/div[2]/div/div[3]/button[2]').click()
             time.sleep(2)
             self.log.info(username + " Success")
             print(username + " [green]Success")
+            return True
         except Exception as e:
             self.log.error(username + " Fail")
             print(username + " [red]Fail")
+            return False
 
     def automaticLogOut(self):
         self.driver.find_element(by=By.XPATH, value='//*[@id="riotbar-account-bar"]/div/div').click()
