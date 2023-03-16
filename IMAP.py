ecf16158afeff9e35d6d241c34b248add073869f
Yudaotor <yudaotor@qq.com>
2023-03-16 14:18:43 +0800

    加入自动获取邮箱验证码功能

---
+++ IMAP.py
@@ -0,0 +1,27 @@
+import email
+from threading import *
+from datetime import datetime
+import re
+
+
+def fetchCode(self):
+    try:
+        status, info = self.M.uid('search', None, 'ALL')
+        if status == "OK":
+            status, info = self.M.uid('fetch', info[0].split()[-1], '(RFC822)')
+            if status == "OK":
+                mail = email.message_from_bytes(info[0][1])
+                if mail['From'].find('noreply@mail.accounts.riotgames.com') > -1:
+                    self.code = re.findall(r'\d{6}', mail["Subject"])[0]
+    except Exception as e:
+        print(e)
+
+
+class IMAP(object):
+    def __init__(self, conn):
+        self.M = conn
+        self.code = ""
+        fetchCode(self)
+
+
+
